9932bf989511e53a32aa36b637f589fa9b3d4f24
Mostafa Said <m.ssaid356@gmail.com>
2025-10-04 14:06:03 +0300

    Add project files.

---
+++ apiContact/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using apiContact.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+namespace apiContact.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
